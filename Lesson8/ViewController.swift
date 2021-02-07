77d7ddb9ebb2074e5cc8a16dab5ce6a3740c3628
Artem Sarana <registrr2009@yandex.ru>
2021-02-07 18:16:11 +0300

    Initial Commit

---
+++ Lesson8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson8
+//
+//  Created by Артём Сарана on 07.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
